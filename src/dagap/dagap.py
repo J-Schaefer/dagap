311d49607090ae8ab1463064e1619593b90bce41
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2021-12-06 17:12:34 +0100

    Minor change
    Include service type in declaration.

--- src/dagap/dagap.py
+++ src/dagap/dagap.py
@@ -1,9 +1,10 @@
 import rospy
 
+from dagap import GetGraspPose
 
 class DAGAP:
     def __init__(self):
-        self.service = rospy.ServiceProxy('srv_dagap')
+        self.service = rospy.ServiceProxy('srv_dagap', GetGraspPose)
         pass
 
     def cb_service(self, req):
