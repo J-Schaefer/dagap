1fc9465ff8af75b2720074f61fb4732bc1a1bd7b
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2022-08-09 17:23:42 +0200

    Add comment to robot module

--- dagappy/src/dagap/tree/robot.py
+++ dagappy/src/dagap/tree/robot.py
@@ -5,8 +5,11 @@ import pathlib
 
 class Robot:
     def __init__(self):
+        self.package_root = pathlib.Path(__file__).resolve().parents[3]
+        self.robot_description_file_path = pathlib.Path(__file__).resolve().parents[3].joinpath("model/robot_description.xml")
+        # TODO: check if folder exists and create
         self.robot = rosparam.get_param(u'robot_description')
-        with open(u'robot_description.xml', 'w') as f:
+        with open(self.robot_description_file_path, 'w') as f:
             f.write(self.robot)
             f.close()
         pass
