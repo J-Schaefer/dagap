bffa916a2a4eeeeea393f4e237ca74e5d20fceb5
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2023-08-21 11:50:16 +0200

    Use right spawning poses
    Also added some type hints and comments.

--- dagap_demo/scripts/demo.py
+++ dagap_demo/scripts/demo.py
@@ -3,6 +3,8 @@
 from __future__ import print_function
 
 import sys
+from typing import List, Type
+
 import rospy
 from dagap_msgs.srv import *
 from dagap_msgs.msg import *
@@ -19,7 +21,6 @@ from pycram.designators.location_designator import *
 from pycram.designators.action_designator import *
 from pycram.enums import Arms
 from pycram.designators.object_designator import *
-from geometry_msgs.msg import Pose
 
 def opm_dagap_client(reference_frame, object_list):
     rospy.wait_for_service('dagap_opm_query')
@@ -37,31 +38,34 @@ if __name__ == "__main__":
     frame = "sink_area_surface"
     init()  # call tfwrapper init()
 
-    object_spawning_poses_sink = [
-        list_to_pose([0, 0, 0], [0, 0, 0, 1]), # robot, position empty
-        list_to_pose([0.2, -0.15, 0.1], [0, 0, 0, 1]), # breakfast-cereal
-        list_to_pose([0.2, -0.35, 0.1], [0, 0, 0, 1]), # cup
-        list_to_pose([0.18, -0.55, 0.1], [0, 0, 0, 1]), # bowl
-        list_to_pose([0.15, -0.4, -0.05], [0, 0, 0, 1]), # spoon
-        list_to_pose([0.07, -0.35, 0.1], [0, 0, 0, 1]) # milk
+    object_spawning_poses_sink: List[Pose] = [
+        list_to_pose([0, 0, 0], [0, 0, 0, 1]),  # robot, position empty
+        list_to_pose([0.2, -0.15, 0.1], [0, 0, 0, 1]),  # breakfast-cereal
+        list_to_pose([0.2, -0.35, 0.1], [0, 0, 0, 1]),  # cup
+        list_to_pose([0.18, -0.55, 0.1], [0, 0, 0, 1]),  # bowl
+        list_to_pose([0.15, -0.4, -0.05], [0, 0, 0, 1]),  # spoon
+        list_to_pose([0.07, -0.35, 0.1], [0, 0, 0, 1])  # milk
     ]
-
-    object_spawning_poses_map = [geometry_msgs.msg.Pose]
 
     # transform_sink_map = lookup_transform(frame, u'map')
 
+    # Hint for type of element
     element: geometry_msgs.msg.Pose
+    # Hint for type of list object_spawning_poses_map
+    object_spawning_poses_map: List[geometry_msgs.msg.PoseStamped] = []
+
+    # Transform poses from iai_kitchen/sink_area_surface to map for correct spawn pose
     for element in object_spawning_poses_sink:
         object_spawning_poses_map.append(transform_pose(element, 'map', 'iai_kitchen/'+frame))
 
-    object_spawning_poses = [
-                             OPMObjectQuery("robot", object_spawning_poses_sink[0]),
-                             OPMObjectQuery("breakfast-cereal", object_spawning_poses_sink[1]),
-                             OPMObjectQuery("cup", object_spawning_poses_sink[2]),
-                             OPMObjectQuery("bowl", object_spawning_poses_sink[3]),
-                             OPMObjectQuery("spoon", object_spawning_poses_sink[4]),
-                             OPMObjectQuery("milk", object_spawning_poses_sink[5])
-                            ]
+    object_spawning_poses: List[OPMObjectQuery] = [
+                                                   OPMObjectQuery("robot", object_spawning_poses_sink[0]),
+                                                   OPMObjectQuery("breakfast-cereal", object_spawning_poses_map[1].pose),
+                                                   OPMObjectQuery("cup", object_spawning_poses_map[2].pose),
+                                                   OPMObjectQuery("bowl", object_spawning_poses_map[3].pose),
+                                                   OPMObjectQuery("spoon", object_spawning_poses_map[4].pose),
+                                                   OPMObjectQuery("milk", object_spawning_poses_map[5].pose)
+                                                  ]
 
     # Set up the bullet world
     world = BulletWorld()
